f7149f409d894c95c8a9c95a79674459c14990bf
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2024-12-18 10:29:59 +0100

    correzione finale

--- js/script.js
+++ js/script.js
@@ -9,39 +9,23 @@ const numeroKm = parseInt(prompt("inserire il numero dei km del proprio tragitto
 
 // ELABORAZIONE
 // calcolo prezzo biglietto dell'utente (salvandomi il dato)
-const multiply = numeroKm * 0.21;
-console.log("Prezzo del biglietto: ", multiply.toFixed(10) + "€");
+const prezzo = numeroKm * 0.21;
 
+console.log("Prezzo del biglietto: ", prezzo.toFixed(2) + "€");
 // applico sconto in base all'età
 // SE utente < 18 anni
-let Sconto20 = multiply / 100 * 20;
-let Sconto40 = multiply / 100 * 40;
-let sottrazione20;
-let sottrazione40;
-
 if (Userage < 18) { 
-    prezzosconto20 = multiply - Sconto20;
-    // ALTRIMENTI SE utente < 65 anni
+    let Sconto20 = prezzo / 100 * 20;
+    prezzosconto20 = prezzo - Sconto20;
+    console.log("Pagherai: ", prezzosconto20.toFixed(2) + "€");
 
-} else if (Userage > 65) {
-    prezzosconto40 = multiply - Sconto40;
+// ALTRIMENTI SE utente >= 65 anni
+} else if (Userage >= 65) {
+    let Sconto40 = prezzo / 100 * 40;
+    prezzosconto40 = prezzo - Sconto40;
+    console.log("Pagherai: ", prezzosconto40.toFixed(2) + "€");
+
+// ALTRIMENTI SE utente > 18 e < 65 anni
+} else {
+    console.log("Pagherai: ", prezzo.toFixed(2) + "€");
 }
-
-// OUTPUT
-// il PC ci restituisce il preventivo del prezzo del biglietto
-if (Userage < 18) { 
-
-    console.log("Preventivo biglietto scontato: ", prezzosconto20);
-    console.log("Pagherai: ", prezzosconto20.toFixed(10) + "€");
-
-// ALTRIMENTI SE utente < 65 anni
-} else if (Userage > 65) {
-    console.log("Preventivo biglietto scontato: ", prezzosconto40 + "€");
-    console.log("Pagherai: ", prezzosconto40.toFixed(10) + "€");
-
-
-// ALTRIMENTI SE utente >18 e < 65 anni
-} else if (Userage > 65 && Userage > 18) {
-    console.log("Preventivo biglietto scontato: ", multiply + "€");
-    console.log("Pagherai: ", multiply.toFixed(10) + "€");
-    }
