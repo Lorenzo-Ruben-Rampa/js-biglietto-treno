f2959110e3c80e54b2a5a6675254f8c20e836e03
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2024-12-18 09:59:20 +0100

    Modifica errore e aggiunta prezzo per maggiorenni < 65

--- js/script.js
+++ js/script.js
@@ -2,44 +2,46 @@
 
 // INPUT
 // l'utente inserisce la usa età
-let Userage = parseInt(prompt("inserire la propria età"));
+const Userage = parseInt(prompt("inserire la propria età"));
 
 // l'utente inserisce i km del percorso del suo viaggio
-let numeroKm = parseInt(prompt("inserire il numero dei km del proprio tragitto"));
+const numeroKm = parseInt(prompt("inserire il numero dei km del proprio tragitto"));
 
 // ELABORAZIONE
 // calcolo prezzo biglietto dell'utente (salvandomi il dato)
-let multiply = numeroKm * 0.21;
+const multiply = numeroKm * 0.21;
 console.log("Prezzo del biglietto: ", multiply);
 
 // applico sconto in base all'età
 // SE utente < 18 anni
-
 let Sconto20 = multiply / 100 * 20;
 let Sconto40 = multiply / 100 * 40;
-
 let sottrazione20;
-if (Userage < 18) { 
-    multiply - Sconto20;
-    // ALTRIMENTI SE utente < 65 anni
 let sottrazione40;
+
+if (Userage < 18) { 
+    prezzosconto20 = multiply - Sconto20;
+    // ALTRIMENTI SE utente < 65 anni
+
 } else if (Userage > 65) {
-    multiply - Sconto40;
+    prezzosconto40 = multiply - Sconto40;
 }
 
 // OUTPUT
 // il PC ci restituisce il preventivo del prezzo del biglietto
-let prezzosconto20;
-let prezzosconto40;
 if (Userage < 18) { 
-    prezzosconto20;
 
 console.log("Preventivo biglietto scontato: ", prezzosconto20);
-let prezzosconto20 = prezzosconto20.toFixed(10);
+console.log("Pagherai: ", prezzosconto20.toFixed(10) + "€");
 
-    // ALTRIMENTI SE utente < 65 anni
+// ALTRIMENTI SE utente < 65 anni
 } else if (Userage > 65) {
-    prezzosconto40;
-}
-console.log("Preventivo biglietto scontato: ", prezzosconto40);
-let prezzosconto40 = prezzosconto40.toFixed(10);
+console.log("Preventivo biglietto scontato: ", prezzosconto40 + "€");
+console.log("Pagherai: ", prezzosconto40.toFixed(10) + "€");
+
+
+// ALTRIMENTI SE utente >18 e < 65 anni
+} else if (Userage > 65 && Userage > 18) {
+    console.log("Preventivo biglietto scontato: ", multiply + "€");
+    console.log("Pagherai: ", multiply.toFixed(10) + "€");
+    }
